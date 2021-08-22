cba8bdc5408d5480cfa7044385d0046abf35beaa
Shinaya123 <83915008+Shinaya123@users.noreply.github.com>
2021-08-22 22:32:20 +0530

    Add files via upload

---
+++ myQuizTemplate-37-main/Quiz.js
@@ -0,0 +1,60 @@
+class Quiz {
+    constructor(){}
+  
+    getState(){
+      var gameStateRef  = database.ref('gameState');
+      gameStateRef.on("value",function(data){
+         gameState = data.val();
+      })
+  
+    }
+  
+    update(state){
+      database.ref('/').update({
+        gameState: state
+      });
+    }
+  
+    async start(){
+      if(gameState === 0){
+        contestant = new Contestant();
+        var contestantCountRef = await database.ref('contestantCount').once("value");
+        if(contestantCountRef.exists()){
+          contestantCount = contestantCountRef.val();
+          contestant.getCount();
+        }
+        question = new Question()
+        question.display();
+      }
+    }
+  
+    play(){
+      question.hide();
+      background("pink");
+      fill('black');
+      textSize(30);
+      text("The Result of The Quiz", 280, 200);
+      text("-------------------------------",280,215);
+      Contestant.getPlayerInfo();
+      if(allContestant!==undefined){
+        var display_answers = 230;
+        fill("black");
+        textSize(20);
+        text("*Note: Contestants who answered correct are highlighted in Green colour!",100,230);
+        for(var plr in allContestant){
+          var correctAns = '2';
+          if(correctAns===allContestant[plr].answer)
+          fill("green");
+          else
+          fill("red");
+          display_answers+=30;
+          textSize(20);
+          text(allContestant[plr].name+":"+allContestant[plr].answer,250,display_answers);
+        }
+      }
+  
+      
+    }
+  
+  }
+  
